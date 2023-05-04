329d452a56dfbd1b0805ece54356a67f6c126d4c
Leonardo Cavalcante <72889853+leocavalca@users.noreply.github.com>
2023-05-03 22:31:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,41 +2,32 @@ const tela = document.getElementById("c_tela");
 const botoes = document.querySelectorAll(".n_btn");
 /*
 document.querySelectorAll retorna uma lista de elementos e não um elemento propriamente dito.
-
 Logo, botoes é um array e não é possível adicionar um eventListener em um array.
-
 Se você quer adicionar o mesmo eventListener para todos os elementos deste array, vai precisar fazer um loop nos elementos e adicioná-lo.
 */
-let calculo;
-let eqPress = false;
-let clPress = false;
-let pValor = true;
 
-botoes.forEach((botao) =>{
-    botao.addEventListener("click", (e) => {
-    const valor = e.target.textContent;
-        // while (eqPress == false){
-        //     calculo = calculo+valor;
-        //     if (valor == "="){
-        //         eqPress == true;
-        //     }
-        // }
+botoes.forEach(botao => {
+	botao.addEventListener('click', () => {
+		const bValor = botao.value;
+		if (bValor === '=') {
+			try {
+				tela.value = eval(tela.value);
+			} catch (error) {
+				tela.value = 'Erro!';
+			}
+		} else if (bValor === 'C') {
+			tela.value = '';
+		} else if (bValor === "Del"){
+            bValor -= bValor;
+            tela.value = bValor;
+        }
+        
+        else {
+			tela.value += bValor;
+		}
+	});
+});
 
-    if (valor == "Limpar"){
-        clPress = true;
-    } 
-        else if (pValor == true) {
-        calculo = valor;
-        pValor == false;
-        tela.value = valor;
-        } 
-
-    if(clPress == true){
-        tela.value = "";
-        clPress = false;
-        pValor = true;
-    }
-})});
 
 
 
